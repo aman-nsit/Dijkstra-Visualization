3fbbc95b4d89f81f7a6f96880b71d955dba60298
aman-nsit <jaibholenath27027@gmail.com>
2023-10-02 21:33:41 +0530

    visualize djkstra

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
+
+function App() {
+  return (
+    <div className="App">
+      <PathfindingVisualizer></PathfindingVisualizer>
+    </div>
+  );
+}
+//shortest path finder app --basically a implementation of dijistras algorithm
+export default App;
